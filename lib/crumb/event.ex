92c10ca7b924a8a6c454591642c162a6263d4348
Doug Wright <mrdougwright@gmail.com>
2025-04-05 23:28:38 -0500

    Fix dup and derive Jason

--- lib/crumb/event.ex
+++ lib/crumb/event.ex
@@ -1,19 +1,5 @@
 defmodule Crumb.Event do
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  schema "events" do
-    field :event, :string
-    field :user_id, :string
-    field :properties, :map
-
-    timestamps(type: :utc_datetime)
-  end
-
-  @doc false
-  def changeset(event, attrs) do
-    event
-    |> cast(attrs, [:event, :user_id, :properties])
-    |> validate_required([:event, :user_id])
-  end
+  @moduledoc """
+  Context for event logic
+  """
 end
--- lib/crumb/events/event.ex
+++ lib/crumb/events/event.ex
@@ -2,12 +2,14 @@ defmodule Crumb.Events.Event do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Jason.Encoder, only: [:event, :properties, :user_id, :inserted_at]}
+
   schema "events" do
     field :event, :string
-    field :user_id, :string
     field :properties, :map
+    field :user_id, :string
 
-    timestamps()
+    timestamps(type: :utc_datetime)
   end
 
   def changeset(event, attrs) do
